9059a63a05a1812413ada7cf90cd9922ae99a02d
hawk0120 <hawk0120@algonquinlive.com>
2023-12-22 00:09:29 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// index.js
+const { app, BrowserWindow, globalShortcut } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  mainWindow.loadFile('index.html');
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  globalShortcut.register('Space', () => {
+    mainWindow.webContents.send('jump');
+  });
+
+  app.on('activate', function () {
+    if (mainWindow === null) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
